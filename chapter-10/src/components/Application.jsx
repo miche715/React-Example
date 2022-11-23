661fda04c55bf61e3f91674a6162bba51ce04831
Cheel <44915367+miche715@users.noreply.github.com>
2022-11-23 13:54:46 +0900

    반복 렌더링 연습

---
+++ chapter-10/src/components/Application.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AttendanceBook from "./AttendanceBook";
+
+function Application()
+{
+    return (
+        <div>
+            <AttendanceBook />
+        </div>
+    );
+}
+export default Application
---
+++ chapter-10/src/components/AttendanceBook.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const students =[
+    {
+        id: 1, 
+        name: "학생 1", 
+    }, 
+    {
+        id: 2, 
+        name: "학생 2", 
+    }, 
+    {
+        id: 3, 
+        name: "학생 3", 
+    }, 
+    {
+        id: 4, 
+        name: "학생 4", 
+    }, 
+];
+
+function AttendanceBook(props)
+{
+    return (
+        <ul>
+            {students.map((student) => {
+                return <li key={`student-${student.id}`}>{student.name}</li>;
+            })}
+        </ul>
+    );
+}
+export default AttendanceBook;
