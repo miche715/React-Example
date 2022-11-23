eea500c6df2fff86f167afe89dc978bb8c55b636
Cheel <44915367+miche715@users.noreply.github.com>
2022-11-23 12:25:07 +0900

    조건부 렌더링 연습

---
+++ chapter-09/src/components/Toolbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const style = {
+    wrapper: {
+        padding: 16, 
+        display: "flex", 
+        flexDirection: "row", 
+        borderBottom: "1px solid grey", 
+    }, 
+    greeting: {
+        marginRight: 8, 
+    }, 
+};
+
+function Toolbar(props)
+{
+    const isLoggedIn = props.isLoggedIn;
+    const onClickLogin = props.onClickLogin;
+    const onClickLogout = props.onClickLogout;
+
+    return (
+        <div style={style.wrapper}>
+            {isLoggedIn
+                ? <span style={style.greeting}>환영</span>
+                : null
+            }
+
+            {isLoggedIn 
+                ? <button onClick={onClickLogout}>로그아웃</button>
+                : <button onClick={onClickLogin}>로그인</button>
+            }
+        </div>
+    );
+}
+export default Toolbar;
