00b9916027ba875d4cebf0d3e55f83b304a51bbb
Cheel <44915367+miche715@users.noreply.github.com>
2022-11-24 10:47:16 +0900

    폼 입력 연습

---
+++ chapter-11/src/components/Application.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "./SignUp";
+
+function Application()
+{
+    return (
+        <div>
+            <SignUp />
+        </div>
+    );
+}
+export default Application
