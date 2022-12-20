7cb1f8326d8d4f40b3dd48fd977b992528f9fb75
Cheel <44915367+miche715@users.noreply.github.com>
2022-12-20 12:17:03 +0900

    chapter-03 실습

---
+++ chapter-03/src/components/Application.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Library from "./Library";
+
+function Application()
+{
+    return (
+        <div>
+            <Library />
+        </div>
+    );
+}
+export default Application
