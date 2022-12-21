1e57a30986bc19924543e7a56b2a3a7f0504eb4b
Cheel <44915367+miche715@users.noreply.github.com>
2022-12-21 15:01:26 +0900

    훅 연습

--- hook-practice/src/components/Count.jsx
+++ hook-practice/src/components/Count.jsx
@@ -11,7 +11,7 @@ function Count(props)
     useEffect(() => {
         if(count !== undefined)
         {
-            console.log(`현재 카운트 = ${count}`);  // 왜 2번 불리지?ㅅㅄㅄㅄㅄㅄㅄㅄㅄㅄㅂㄳㅂ??모르겠다 아니 이제 알겠다. 마운트될때랑 0으로 초기화될 때 호출되는거엿네;
+            console.log(`현재 카운트 = ${count}`);
         }
         else
         {
